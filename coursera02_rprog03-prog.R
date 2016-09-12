c06a5645defb9312189c235a8fc3124140835f0d
Nils Matzner <nilsmatzner@users.noreply.github.com>
2016-09-12 16:19:01 +0200

    Create coursera02_rprog03-prog.R

---
+++ coursera02_rprog03-prog.R
@@ -0,0 +1,36 @@
+## Programming assignment2
+## https://github.com/rdpeng/ProgrammingAssignment2
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+
+## Write two functions
+
+# makeCacheMatrix <- function() {}
+
+
+# cacheSolve <- function() {}
+
